4e0d46a7355b6ee142a6f3f9488aa26e2de51e99
kuro-shiv <dubeysk2003@gmail.com>
2026-01-08 00:30:49 +0530

    need work last edit not completed db connection

--- routes/auth.Routes.js
+++ routes/auth.Routes.js
@@ -1,11 +1,14 @@
-import express from "express";
-import { login } from "../controllers/auth.controller.js";
+import express from "express"
+import { login, register } from "../controllers/auth.controller.js"
+import { protect } from "../middlewares/auth.middleware.js"
 
+const router = express.Router()
 
-const router = express.Router();
+router.post("/register", register)
+router.post("/login", login)
 
-
-router.post("/login", login);
-
+router.get("/me", protect, (req, res) => {
+  res.json(req.user)
+})
 
 export default router
--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-
+import connectDB from "./config/db.js"
 import authRoutes from "./routes/auth.Routes.js"
 
 dotenv.config()
+connectDB()
 
-const app =express()
-const PORT =process.env.PORT || 5000
+const app = express()
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("Cherrybot Backend is running!")
+  res.send("Cherrybot backend is running 🍒")
 })
-
 
 app.use("/api/auth", authRoutes)
 
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
