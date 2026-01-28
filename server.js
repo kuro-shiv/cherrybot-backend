65ba2af235b3141382afdcdf1f54ed74bc6c9311
kuro-shiv <dubeysk2003@gmail.com>
2026-01-28 09:09:07 +0530

    start

--- server.js
+++ server.js
@@ -1,28 +1,4 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import connectDB from "./config/db.js"
-import authRoutes from "./routes/auth.routes.js"
-import chatRoutes from "./routes/chat.routes.js"
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
-
-dotenv.config()
-connectDB()
-
-const app = express()
-const PORT = process.env.PORT || 5000
-
-app.use(cors())
-app.use(express.json())
-
-app.get("/", (req, res) => {
-  res.send("Cherrybot backend is running 🍒")
-})
-
-app.use("/api/chat", chatRoutes)
-
-app.use("/api/auth", authRoutes)
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
